3a305a2a30c54e7e6cdbb78b67764008a99cc07b
johnclark96 <mlitvinkox96@gmail.com>
2021-09-04 16:23:51 -0400

    Added Avatar and code planning

--- app/src/main/java/com/github/johnclark96/improvepets/MainActivity.kt
+++ app/src/main/java/com/github/johnclark96/improvepets/MainActivity.kt
@@ -8,4 +8,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//    val anim = Dice(6)
+//    val animRoll = anim.roll()
+//
+//    private fun avatarAnim() {
+//        val drawableResource = when (animRoll) {
+//
+//        }
+//    }
 }
+
+//class Avatar () {
+//
+//}
+
+//class Walk (private val walkStatus: Boolean) {
+//
+//    fun walkCounter(): Int {
+//        return 0;
+//    }
+//}
+
+//class Dice (private val numberAnims: Int) {
+//
+//    //Random Dice Roll and Return Result
+//    fun roll(): Int {
+//        return 1++;
+//    }
+//
+//}
